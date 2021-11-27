12a0b13ff3fa2ebdc77ac36c85a78ab6a8600b3c
mickonis <mickonis.l@gmail.com>
2021-11-27 16:42:55 +0200

    darft fetch request

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import { useEffect } from "react";
+
 export const App = function App() {
+ 
+  useEffect(() => {
+getData()
+  },[])
+
+  const getData = async() => {
+    const response = await fetch("http://localhost:3001/api/v1/tree");
+    const json = await response.json();
+    console.log('json',json)
+  }
+
   return <h1>Corti Frontend Home Assignment</h1>;
 };
