8369acd28430634763d4188958ac6aa07b8ce9ee
mickonis <mickonis.l@gmail.com>
2021-11-27 18:19:49 +0200

    setting tree state

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export const App = function App() {
+  const [tree, setTree] = useState<any>(null);
+
   useEffect(() => {
     getData();
   }, []);
@@ -8,8 +10,10 @@ export const App = function App() {
   const getData = async () => {
     const response = await fetch('http://localhost:3001/api/v1/tree');
     const json = await response.json();
-    console.log('json', json);
+    setTree(json.response);
   };
+
+  console.log('tree', tree);
 
   return <h1>Corti Frontend Home Assignment</h1>;
 };
