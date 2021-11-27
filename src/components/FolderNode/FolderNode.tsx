6c5a5f9f714587e44e730114939ce289c1177998
mickonis <mickonis.l@gmail.com>
2021-11-27 19:59:36 +0200

    added FolderNode style module

--- src/components/FolderNode/FolderNode.tsx
+++ src/components/FolderNode/FolderNode.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-
+import styles from './FolderNode.module.scss';
 interface FileNodeProps {
   name: string;
   children: any;
@@ -8,7 +8,7 @@ interface FileNodeProps {
 const FolderNode = ({ name, children }: FileNodeProps) => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="folder-node" onClick={() => setIsOpen(!isOpen)}>
+    <div className={styles.folder} onClick={() => setIsOpen(!isOpen)}>
       <div>{name}</div>
       {isOpen && <div>{children}</div>}
     </div>
