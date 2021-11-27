c85c56b732533d2f52e85b71f80876a8a91c7319
mickonis <mickonis.l@gmail.com>
2021-11-27 20:13:46 +0200

    added typography styles and variables

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,9 @@
 import style from './Header.module.scss';
 
-const Header = () => <div className={style.header}>Home Assignment</div>;
+const Header = () => (
+  <div className={style.header}>
+    <h1 className={style.title}>Home Assignment</h1>
+  </div>
+);
 
 export default Header;
