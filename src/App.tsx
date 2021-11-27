2e6874231fc549b4e153120f4f0f274b3ab89916
mickonis <mickonis.l@gmail.com>
2021-11-27 16:45:31 +0200

    eslint formating

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
 export const App = function App() {
- 
   useEffect(() => {
-getData()
-  },[])
+    getData();
+  }, []);
 
-  const getData = async() => {
-    const response = await fetch("http://localhost:3001/api/v1/tree");
+  const getData = async () => {
+    const response = await fetch('http://localhost:3001/api/v1/tree');
     const json = await response.json();
-    console.log('json',json)
-  }
+    console.log('json', json);
+  };
 
   return <h1>Corti Frontend Home Assignment</h1>;
 };
